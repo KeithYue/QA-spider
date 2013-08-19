82432d8cc4cdfae0185f37847a49acd18a6f696e
KeithYue <buaawangyue@gmail.com>
2013-08-20 00:53:24 +0800

    add scrapy examples and tutorials

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,23 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from tutorial.items import DemozItem
+
+class DmozSpider(BaseSpider):
+    name = "domz"
+    allowed_domains = ["doma.org"]
+    start_urls = [
+            "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+            "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+            ]
+
+    def parse(self, response):
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//ul/li')
+        items = []
+        for site in sites:
+            item  = DemozItem()
+            item['title'] = site.select('a/text()').extract()
+            item['link'] = site.select('a/@href').extract()
+            item['desc'] = site.select('text()').extract()
+            items.append(item)
+        return items
