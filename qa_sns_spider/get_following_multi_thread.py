2f35d93c75e47d973d53c96f05617e6ac0be0521
KeithYue <buaawangyue@gmail.com>
2013-11-04 15:21:11 +0800

    modify the multi following spider to be controled by queue

--- qa_sns_spider/get_following_multi_thread.py
+++ qa_sns_spider/get_following_multi_thread.py
@@ -2,8 +2,8 @@ from threading import Thread, Lock
 from follow_beta1 import *
 import os
 import json
-
-
+import Queue
+from archiver_beta1 import get_auths_data as archive_auths
 
 def load_auths():
     auth_file = open('./auth_users_follow', 'r')
@@ -15,44 +15,66 @@ def load_auths():
 
 
 class FollowingSpider(Thread):
-    def __init__(self, twitter_username):
-        super(FollowingSpider, self).__init__()
-        self.twitter_username = twitter_username
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
 
     def run(self):
-        global auth_pointer
+        while True:
+            global auth_pointer # pointer of archiever
+            global following_auth_pointer # pointer of following app
+            global is_using_archieve
 
-        # lock the auth pointer for distribution
-        AUTH_LOCK.acquire()
-        print 'I am starting, using %d auther' % \
-                auth_pointer
-        auth = OAuth(auth_users[auth_pointer][0],
-                auth_users[auth_pointer][1],
-                CONSUMER_KEY, CONSUMER_SECRET)
-        auth_pointer = (auth_pointer + 1) % len(auth_users)
-        AUTH_LOCK.release()
+            self.twitter_username = self.queue.get()[1] # get a task
+            print 'I am crawling the number %d user!!' % self.queue.get()[0]
 
-        twitter = Twitter(auth=auth, api_version='1.1',
-                domain='api.twitter.com')
-        filename = './data/twitter-follow/' + os.sep + \
-                self.twitter_username
-        follow_file = open(filename, 'w')
-        user_ids, users = [], {}
-        try:
-            user_ids = follow(twitter, self.twitter_username , True)
-            users = lookup(twitter, user_ids)
-        except KeyboardInterrupt as e:
-            err()
-            err("Interrupted.")
-            raise SystemExit(1)
+            APP_SWITCH.acquire() # get the which app to use
+            if(is_using_archieve): # use twitter archiever
+                AUTH_LOCK.acquire()
+                print 'I am starting, using %d auther' % \
+                        auth_pointer
+                auth = OAuth(archive_auths_users[auth_pointer][0],
+                        archive_auths_users[auth_pointer][1],
+                        CONSUMER_KEY, CONSUMER_SECRET)
+                auth_pointer = (auth_pointer + 1) % len(archive_auths_users)
+                AUTH_LOCK.release()
+                is_using_archieve = False
+            else: # use twitter following
+                FOLLOW_AUTH_LOCK.acquire()
+                print 'I am starting, using %d following auth user' % \
+                        (following_auth_pointer)
+                auth = OAuth(following_auth_users[following_auth_pointer][0],
+                        following_auth_users[following_auth_pointer][1],
+                        FOLLOW_CONSUMER_KEY,
+                        FOLLOW_CONSUMER_SECRET)
+                following_auth_pointer = (following_auth_pointer + 1) % len(following_auth_users)
+                FOLLOW_AUTH_LOCK.release()
+                is_using_archieve = True
+            APP_SWITCH.release()
 
-        for uid in user_ids:
-          try:
-              follow_file.write('%s\t%s\n' % (str(uid), users[uid].encode("utf-8")))
-              # print(str(uid) + "\t" + users[uid].encode("utf-8"))
-          except KeyError:
-              pass
-        follow_file.close()
+            twitter = Twitter(auth=auth, api_version='1.1',
+                    domain='api.twitter.com')
+            filename = './data/twitter-follow/' + os.sep + \
+                    self.twitter_username
+            follow_file = open(filename, 'w')
+            user_ids, users = [], {}
+            try:
+                user_ids = follow(twitter, self.twitter_username , True)
+                users = lookup(twitter, user_ids)
+            except KeyboardInterrupt as e:
+                err()
+                err("Interrupted.")
+                raise SystemExit(1)
+
+            for uid in user_ids:
+              try:
+                  follow_file.write('%s\t%s\n' % (str(uid), users[uid].encode("utf-8")))
+                  # print(str(uid) + "\t" + users[uid].encode("utf-8"))
+              except KeyError:
+                  pass
+            follow_file.close()
+
+            self.queue.task_done() # indicate the task is finished
 
 def get_following_data(thread_number = 5):
     total_number = len(users_list)
@@ -62,11 +84,25 @@ def get_following_data(thread_number = 5):
         for i in range(0, thread_number):
             if index < total_number:
                 thread_list.append(FollowingSpider(users_list[index]))
+                print 'Now I am the number %d user!!' % index
                 index += 1
         for spider in thread_list:
             spider.start()
         for spider in thread_list:
             spider.join()
+
+# multi-thread spiders but using a queue to manage all threads
+def get_following_data_using_queue():
+    total_number = len(users_list)
+    # print total_number
+    for i in range(max_thead_number): # start total tasks, the number here is the max-size of threads
+        t = FollowingSpider(thread_queue)
+        t.daemon = True
+        t.start()
+    for index, user in enumerate(users_list):
+        thread_queue.put((index, user))
+    thread_queue.join()
+
 
 def load_user_list(filename):
     global users_list
@@ -74,14 +110,40 @@ def load_user_list(filename):
         item = json.loads(line.strip())
         users_list.append(item['twitter_username'][1:])
 
+def load_quora_user_list(filename):
+    global users_list
+    for line in open(filename).xreadlines():
+        twitter_id, twitter_username = line.strip().split('\t')
+        # print line.strip().split('\t')
+        # print twitter_username
+        users_list.append(twitter_username)
+
+# T-Archiver (Twitter-Archiver) application registered by @stalkr_
+CONSUMER_KEY = 'd8hIyfzs7ievqeeZLjZrqQ'
+CONSUMER_SECRET = 'AnZmK0rnvaX7BoJ75l6XlilnbyMv7FoiDXWVmPD8'
+
 # T-Follow (Twitter-Follow) application registered by @stalkr_
-CONSUMER_KEY = 'USRZQfvFFjB6UvZIN2Edww'
-CONSUMER_SECRET = 'AwGAaSzZa5r0TDL8RKCDtffnI9H9mooZUdOa95nw8'
-auth_users = load_auths()
+FOLLOW_CONSUMER_KEY = 'USRZQfvFFjB6UvZIN2Edww'
+FOLLOW_CONSUMER_SECRET = 'AwGAaSzZa5r0TDL8RKCDtffnI9H9mooZUdOa95nw8'
+
+following_auth_users = load_auths()
+archive_auths_users = archive_auths()
 users_list = []
 auth_pointer = 0
+following_auth_pointer = 0
+is_using_archieve = True
 AUTH_LOCK = Lock()
+FOLLOW_AUTH_LOCK = Lock()
+APP_SWITCH = Lock()
+
+# Use queue to set up deamon theads pool
+
+max_thead_number = 8
+thread_queue = Queue.Queue(maxsize = max_thead_number)
 
 if __name__ == '__main__':
-    load_user_list('./data/stack_twitter_link.json')
-    get_following_data()
+    load_quora_user_list('./data/quora_id_twitter')
+    # print users_list
+    # load_user_list('./data/stack_twitter_link.json')
+    # get_following_data(thread_number = 8)
+    get_following_data_using_queue()
